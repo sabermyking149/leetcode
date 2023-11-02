056a302660e4b98f443599d68e497d5a884d07fd
sabermyking149 <sabermyking149@gmail.com>
2023-11-02 22:05:56 +0800

    add function calculateMinimumHP

--- learning/src/leetcode.cpp
+++ learning/src/leetcode.cpp
@@ -7648,3 +7648,28 @@ TreeNode* upsideDownBinaryTree(TreeNode* root)
     }
     return ans;
 }
+
+
+// LC174
+// dp[i][j] - 从{i, j}走到终点的最小健康点数
+int calculateMinimumHP(vector<vector<int>>& dungeon)
+{
+    int i, j;
+    auto m = dungeon.size();
+    auto n = dungeon[0].size();
+    vector<vector<int>> dp(m, vector<int>(n));
+
+    dp[m - 1][n - 1] = dungeon[m - 1][n - 1] <= 0 ? abs(dungeon[m - 1][n - 1]) + 1 : 1;
+    for (j = n - 2; j >= 0; j--) {
+        dp[m - 1][j] = max(1, dp[m - 1][j + 1] - dungeon[m - 1][j]);
+    }
+    for (i = m - 2; i >= 0; i--) {
+        dp[i][n - 1] = max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+    }
+    for (i = m - 2; i >= 0; i--) {
+        for (j = n - 2; j >= 0; j--) {
+            dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+        }
+    }
+    return dp[0][0];
+}
